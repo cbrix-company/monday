abb412a21dff73b55cfc244e4bf8035dc94650d9
David Melamed <dvdmelamed@gmail.com>
2020-09-15 14:10:00 +0300

    Fix urllib import for python3

--- monday/graphqlclient/client.py
+++ monday/graphqlclient/client.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib.request
 import json
 
 
@@ -11,25 +11,25 @@ class GraphQLClient:
     def execute(self, query, variables=None):
         return self._send(query, variables)
 
-    def inject_token(self, token, headername='Authorization'):
+    def inject_token(self, token, headername="Authorization"):
         self.token = token
         self.headername = headername
 
     def _send(self, query, variables):
-        data = {'query': query,
-                'variables': variables}
-        headers = {'Accept': 'application/json',
-                   'Content-Type': 'application/json'}
+        data = {"query": query, "variables": variables}
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
         if self.token is not None:
-            headers[self.headername] = '{}'.format(self.token)
+            headers[self.headername] = "{}".format(self.token)
 
-        req = urllib.request.Request(self.endpoint, json.dumps(data).encode('utf-8'), headers)
+        req = urllib.request.Request(
+            self.endpoint, json.dumps(data).encode("utf-8"), headers
+        )
 
         try:
             response = urllib.request.urlopen(req)
-            return response.read().decode('utf-8')
+            return response.read().decode("utf-8")
         except urllib.error.HTTPError as e:
             print((e.read()))
-            print('')
+            print("")
             raise e
